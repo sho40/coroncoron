f3073b57f90b3ab3859f35f1a2a4057b0c59c937
sho40 <kanazawa.sho40@gmail.com>
2020-05-07 02:49:54 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "homes#index"
+  
+  # Top画面
+  resources :homes, only: [:index]
 end
