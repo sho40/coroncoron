210b415da1ebbf03afd37c66f31c20d857c97900
takalivian <61145524+takalivian@users.noreply.github.com>
2020-05-09 14:56:21 +0900

    db Talent

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_09_043955) do
+ActiveRecord::Schema.define(version: 2020_05_09_052054) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "namespace"
@@ -41,6 +41,27 @@ ActiveRecord::Schema.define(version: 2020_05_09_043955) do
   create_table "news", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "category"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "talents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "f_name"
+    t.string "l_name"
+    t.string "e_f_name"
+    t.string "e_l_name"
+    t.string "sex"
+    t.date "dob"
+    t.string "blood"
+    t.string "height"
+    t.string "weight"
+    t.string "chest"
+    t.string "waist_bust"
+    t.string "hips"
+    t.string "shoes"
+    t.string "birthplace"
+    t.string "role"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
