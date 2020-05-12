2a036d6d3823871c015dee6ac2a3e90c637a79e0
sho40 <kanazawa.sho40@gmail.com>
2020-05-12 11:48:56 +0900

    add date column

--- app/admin/news.rb
+++ app/admin/news.rb
@@ -1,9 +1,10 @@
 ActiveAdmin.register News do
 
-  permit_params :category, :text
+  permit_params :date, :category, :text
 
   form do |f|
     f.inputs do
+      f.date_select :date
       f.input :category, as: :select, collection: ['テレビ', '雑誌', '舞台', 'イベント'], prompt: "選択してください"
       f.input :text
     end
