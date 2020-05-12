f9eb60087babfa172b3c564d0e9b077b60f52e69
sho40 <kanazawa.sho40@gmail.com>
2020-05-12 11:54:56 +0900

    Update news.rb

--- app/admin/news.rb
+++ app/admin/news.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register News do
   form do |f|
     f.inputs do
       f.date_select :date
-      f.input :category, as: :select, collection: ['テレビ', '雑誌', '舞台', 'イベント'], prompt: "選択してください"
+      f.input :category, as: :select, collection: ['TV', '雑誌', '舞台', 'イベント'], prompt: "選択してください"
       f.input :text
     end
     f.actions
