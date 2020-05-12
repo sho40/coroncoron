26e2bf5bcebe1d9256064afd8cf19521505a5d19
sho40 <kanazawa.sho40@gmail.com>
2020-05-12 11:50:12 +0900

    add colmn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_09_043955) do
+ActiveRecord::Schema.define(version: 2020_05_11_093432) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "namespace"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_05_09_043955) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date"
   end
 
 end
