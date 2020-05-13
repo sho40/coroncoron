3bd9e0a7a4ad8e5c4d33f43637efc651d73f27e0
takalivian <61145524+takalivian@users.noreply.github.com>
2020-05-13 21:50:16 +0900

    画像表示

--- app/controllers/talents_controller.rb
+++ app/controllers/talents_controller.rb
@@ -4,5 +4,12 @@ class TalentsController < ApplicationController
   end
 
   def show
+    @talents = Talent.all.order(created_at: "ASC")
+    @talent = Talent.find(params[:id])
+  end
+
+  private
+  def item_params
+    params.require(:talent).permit(:f_name, :l_name, :e_f_name, :e_l_name, :sex, :dob, :blood, :height, :weight, :chest, :waist_bust, :hips, :shoes, :birthplace, :role, :body, images_attributes: [:_destroy,:id,:url])
   end
 end
